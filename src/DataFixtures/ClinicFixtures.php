943e770cc84f0a243bb66632dc61051bfbfa17ac
Nasiba <wwerquter@gmail.com>
2024-04-22 22:32:15 +0500

    fixtures

--- src/DataFixtures/ClinicFixtures.php
+++ src/DataFixtures/ClinicFixtures.php
@@ -17,79 +17,79 @@ class ClinicFixtures extends Fixture
 {
     private array $categories = [
         [
-            'name' => 'Стоматологические клиники',
-            'description' => 'Стоматологические клиники предлагают полный спектр услуг для заботы о вашей улыбке. От профессиональной гигиены и лечения кариеса до ортодонтии и имплантологии, мы обеспечим вашим зубам здоровье и красоту.',
+            'name' => 'Dental clinics',
+            'description' => 'Dental clinics offer a full range of services to take care of your smile. From professional hygiene and caries treatment to orthodontics and implantation, we will ensure your teeth are healthy and beautiful.',
         ],
-        [   "name" => 'Онкологические клиники',
-            'description' => 'В онкологических клиниках работают врачи-онкологи, специализирующиеся на диагностике, лечении и реабилитации пациентов с онкологическими заболеваниями. Мы предоставляем индивидуальный подход и передовые методы лечения для достижения наилучших результатов.',
-        ],
-        [
-            "name" => 'Терапевтические клиники',
-            'description' => 'Терапевтические клиники предлагают широкий спектр медицинских услуг, включая диагностику и лечение различных заболеваний. Наши врачи-терапевты обеспечат вас качественной медицинской помощью и заботой о вашем здоровье.',
+        [   "name" => 'Oncology clinics',
+            'description' => 'Oncology clinics employ oncologists who specialize in the diagnosis, treatment and rehabilitation of patients with cancer. We provide personalized attention and advanced treatment methods to achieve the best results.',
         ],
         [
-            "name" => 'Травматологические клиники',
-            'description' => 'В травматологических клиниках работают специалисты по лечению травм и ортопедических заболеваний. Мы предлагаем комплексный подход к лечению травм, реабилитации и восстановлению функций опорно-двигательной системы.',
+            "name" => 'Therapy clinics',
+            'description' => 'Therapy clinics offer a wide range of medical services, including diagnosis and treatment of various diseases. Our therapists will provide you with quality medical care and care for your health.',
         ],
         [
-            "name" => 'Хирургические клиники',
-            'description' => 'Хирургические клиники предоставляют услуги по плановым и экстренным операциям различного характера. Наши высококвалифицированные хирурги обеспечат вас безопасной и эффективной хирургической помощью.',
+            "name" => 'Trauma clinics',
+            'description' => 'Trauma clinics employ specialists in the treatment of trauma and orthopedic diseases. We offer a comprehensive approach to the treatment of injuries, rehabilitation and restoration of functions of the musculoskeletal system.',
         ],
         [
-            "name" => 'Лабораторно-испытательные клиники',
-            'description' => 'Лабораторно-испытательные клиники предлагают широкий спектр лабораторных исследований для диагностики различных заболеваний. Мы используем передовые технологии и методики для точной и быстрой диагностики.',
+            "name" => 'Surgical clinics',
+            'description' => 'Surgical clinics provide services for planned and emergency operations of various types. Our highly trained surgeons will provide you with safe and effective surgical care.',
         ],
         [
-            "name" => 'Эндоскопические клиники',
-            'description' => 'Эндоскопические клиники специализируются на диагностике и лечении заболеваний с использованием эндоскопических методов. Наши специалисты проведут процедуры с максимальным комфортом и минимальными рисками для пациента.',
+            "name" => 'Laboratory testing clinics',
+            'description' => 'Laboratory testing clinics offer a wide range of laboratory tests for the diagnosis of various diseases. We use advanced technologies and techniques for accurate and fast diagnosis.',
         ],
         [
-            "name" => 'Инфекционные клиники',
-            'description' => 'Инфекционные клиники специализируются на диагностике, лечении и профилактике инфекционных заболеваний. Наши врачи обеспечат вас компетентной помощью и рекомендациями по предотвращению инфекций.',
+            "name" => 'Endoscopy clinics',
+            'description' => 'Endoscopic clinics specialize in the diagnosis and treatment of diseases using endoscopic methods. Our specialists will perform procedures with maximum comfort and minimal risks for the patient.',
         ],
         [
-            "name" => 'Клиники вакцинации',
-            'description' => 'Клиники вакцинации предлагают широкий выбор вакцин для профилактики различных инфекционных заболеваний. Мы гарантируем качественные вакцины и профессиональное введение с учетом всех медицинских стандартов.',
+            "name" => 'Infectious diseases clinics',
+            'description' => 'Infectious diseases clinics specialize in the diagnosis, treatment and prevention of infectious diseases. Our doctors will provide you with competent care and recommendations to prevent infections.',
+        ],
+        [
+            "name" => 'Vaccination clinics',
+            'description' => 'Vaccination clinics offer a wide range of vaccines to prevent various infectious diseases. We guarantee high-quality vaccines and professional administration, taking into account all medical standards.',
         ],
     ];
 
-    private array$services = [
+    private array $services = [
         [
-            'name' => 'Общий врач',
+            'name' => 'General doctor',
         ],
         [
-            'name' => 'Хирург',
+            'name' => 'Surgeon',
         ],
         [
-            'name' => 'Терапевт',
+            'name' => 'Therapist',
         ]
     ];
     private array $clinics = [
         [
-            'name' => 'Doktor & Animal',
+            'name' => 'Doctor & Animal',
             'phone' => '(998) 71-268-40-26',
-            'address' => 'Узбекистан, Ташкент, МИРЗО-УЛУГБЕКСКИЙ РАЙОН, просп. МИРЗО УЛУГБЕКА, 40',
-            'description' => 'Реализация товаров для животных, аквариумных рыб, попугаев (аксессуары, корма, 
-                витаминные добавки, парфюмерия, одежда, обувь, литература). Имеется терминал.',
+            'address' => 'Uzbekistan, Tashkent, MIRZO-ULUGBEK DISTRICT, ave. MIRZO ULUGBEK, 40',
+            'description' => 'Sales of goods for animals, aquarium fish, parrots (accessories, food,
+                vitamin supplements, perfumes, clothing, shoes, literature). There is a terminal.',
             'image' => __DIR__ . '/img/05.jpeg',
             'employee' => [
                 0 => [
-                    'userName' => 'Темур',
-                    'password' => 'темур',
+                    'userName' => 'Temur',
+                    'password' => 'temur',
                     'phone' => '+998 (90) 222-11-12',
                     'price' => '50000',
                     'image' => __DIR__ . '/img/1.JPG',
                 ],
                 1 => [
-                    'userName' => 'Акбар',
-                    'password' => 'акбар',
+                    'userName' => 'Akbar',
+                    'password' => 'akbar',
                     'phone' => '+998 (90) 222-11-20',
                     'price' => '60000',
                     'image' => __DIR__ . '/img/01.jpeg',
                 ],
                 2 => [
-                    'userName' => 'Азиз',
-                    'password' => 'азиз',
+                    'userName' => 'Aziz',
+                    'password' => 'aziz',
                     'phone' => '+998 (90) 222-11-30',
                     'price' => '70000',
                     'image' => __DIR__ . '/img/2.JPG',
@@ -99,37 +99,37 @@ class ClinicFixtures extends Fixture
         [
             'name' => 'Doctor Vet ООО',
             'phone' => '(998) 71-260-11-26',
-            'address' => 'Узбекистан, 100204, Ташкент, ЯШНАБАДСКИЙ РАЙОН, м-в АВИАСОЗЛАР-2, 56',
-            'description' => 'Наша компания ведет свою деятельность в протяжении 15 лет и является эксклюзивным дилерам мировых брендов!
-                Мы предлагаем вам широкий ассортимент от мировых брендов:
-                    1. Ветеринарные препараты
-                    2. Корма для животных, птиц и рыб.
-                    3. Аксессуары для домашних животных.
-                    4. Вакцины для животных.
-                    5. Витаминно-минеральные комплексы для животных
-                    6. Пестициды и удобрения
-                    7. Кормушки и поилки для домашних птиц....
-                Одним словом вы можете найти у нас все для защиты и содержания домашних и сельскохозяйственных животных и даже для защиты растений!
-                Цены для оптовиков и для розничной торговли! Индивидуальный подход к каждому потребителю!',
+            'address' => 'Uzbekistan, 100204, Tashkent, YASHNABAD DISTRICT, AVIASOZLAR-2 massif, 56',
+            'description' => 'Our company has been operating for 15 years and is an exclusive dealer of world brands!
+                We offer you a wide range of world brands:
+                    1. Veterinary drugs
+                    2. Food for animals, birds and fish.
+                    3. Accessories for pets.
+                    4. Vaccines for animals.
+                    5. Vitamin and mineral complexes for animals
+                    6. Pesticides and fertilizers
+                    7. Feeders and drinkers for poultry....
+                In a word, you can find with us everything for the protection and maintenance of domestic and farm animals and even for plant protection!
+                Prices for wholesalers and retailers! Individual approach to each consumer!',
             'image' => __DIR__ . '/img/06.jpeg',
             'employee' => [
                 0 => [
-                    'userName' => 'Лола',
-                    'password' => 'лола',
+                    'userName' => 'Lola',
+                    'password' => 'lola',
                     'phone' => '+998 (90) 222-11-40',
                     'price' => '80000',
                     'image' => __DIR__ . '/img/02.webp',
                 ],
                 1 => [
-                    'userName' => 'Анвар',
-                    'password' => 'анвар',
+                    'userName' => 'Anvar',
+                    'password' => 'anvar',
                     'phone' => '+998 (90) 222-11-50',
                     'price' => '90000',
                     'image' => __DIR__ . '/img/3.JPG',
                 ],
                 2 => [
-                    'userName' => 'Муниса',
-                    'password' => 'муниса',
+                    'userName' => 'Munisa',
+                    'password' => 'munisa',
                     'phone' => '+998 (90) 222-11-60',
                     'price' => '100000',
                     'image' => __DIR__ . '/img/03.jpeg',
@@ -139,35 +139,35 @@ class ClinicFixtures extends Fixture
         [
             'name' => 'Zoo Doctor',
             'phone' => '(998) 97-754-54-50',
-            'address' => 'г. Ташкент, Мирабадский район, ул. Миробод, д. 33',
-            'description' => 'Клиника Zoo Doctor - место, где не только помогут и вылечат, но и подарят вашему питомцу 
-                доброту и ласку. Любовь к животным, а также забота о них – то, без чего нашей команды врачей не существовало бы.
-                Представьте, что наши усатые-полосатые пациенты могут получить высококвалифицированную помощь, 
-                начинающуюся от консультации терапевта и заканчивающуюся их комфортабельной транспортировкой. 
-                Современное оборудование, оснащенные иностранной техникой лаборатории и грамотные специалисты помогают 
-                выявить любое заболевание и способствовать его устранению ради здоровой и полноценной жизни животного. 
-                Команда специалистов выезжает даже на дом, создавая максимальный комфорт маленьким пациентам.
-                Для настоящего счастья после выздоровления, мы предлагаем стрижку и купание вашего питомца, а так же полезные, 
-                не дорогие корма и лакомства для вашего любимца. Мы не дарим здоровье, Мы его Сохраняем. С любовью, Zoo Doctor.',
+            'address' => 'Tashkent, Mirabad district, st. Mirobod, 33',
+            'description' => 'Zoo Doctor Clinic is a place where you will not only help and treat, but also give your pet a gift
+                kindness and affection. Love for animals and care for them is something without which our team of doctors would not exist.
+                Imagine that our mustachioed and striped patients can receive highly qualified care,
+                starting from a consultation with a therapist and ending with their comfortable transportation.
+                Modern equipment, laboratories equipped with foreign technology and competent specialists help
+                identify any disease and help eliminate it for the sake of a healthy and fulfilling life for the animal.
+                A team of specialists even visits home, creating maximum comfort for young patients.
+                For real happiness after recovery, we offer haircuts and bathing for your pet, as well as useful,
+                inexpensive food and treats for your pet. We do not give health, We preserve it. With love, Zoo Doctor.',
             'image' => __DIR__ . '/img/07.jpeg',
             'employee' => [
                 0 => [
-                    'userName' => 'Бахром',
-                    'password' => 'бахром',
+                    'userName' => 'Bahrom',
+                    'password' => 'bahrom',
                     'phone' => '+998 (90) 222-11-72',
                     'price' => '110000',
                     'image' => __DIR__ . '/img/4.jpeg',
                 ],
                 1 => [
-                    'userName' => 'Мадина',
-                    'password' => 'мадина',
+                    'userName' => 'Madina',
+                    'password' => 'madina',
                     'phone' => '+998 (90) 222-11-80',
                     'price' => '120000',
                     'image' => __DIR__ . '/img/04.JPG',
                 ],
                 2 => [
-                    'userName' => 'Луиза',
-                    'password' => 'луиза',
+                    'userName' => 'Luiza',
+                    'password' => 'luiza',
                     'phone' => '+998 (90) 222-11-90',
                     'price' => '130000',
                     'image' => __DIR__ . '/img/5.JPG',
@@ -177,35 +177,35 @@ class ClinicFixtures extends Fixture
         [
             'name' => 'Пес и Кот',
             'phone' => '(998) 90-320-58-33',
-            'address' => 'Якуба Коласа, 4',
-            'description' => 'В зоомагазине «Пес и Кот» мы рады приветствовать всех посетителей, владельцев разнообразных домашних животных.
-                На полках магазина вы найдёте широкий ассортимент кормов для птиц, рыб, кошек, собак, а также любые 
-                аксессуары для ухода за животными и организации комфортной жизни своих любимцев. Опытные продавцы-консультанты 
-                готовы ответить на все ваши вопросы. Все зоотовары и корма проходят обязательную и тщательную проверку.
-                Мы работаем только с официальными дистрибьютерами кормов, именно поэтому можем гарантировать качество.
-                Для постоянных клиентов предусмотрена гибкая система скидок. В нашем салоне работают профессиональные 
-                грумеры умеющие найти подход к Вашем питомцам. Владеющие знаниями анатомии и психологии животных.
-                Мы работаем только с профессиональной косметикой. Собаки, подготовленные в нашем салоне, становятся 
-                победителями выставок. Красота и здоровье шерсти Ваших питомцев в наших надёжных руках!',
+            'address' => 'Yakub Kolas, 4',
+            'description' => 'At the pet store "Dog and Cat" we are glad to welcome all visitors, owners of a variety of pets.
+                On the store shelves you will find a wide range of food for birds, fish, cats, dogs, as well as any
+                accessories for caring for animals and organizing a comfortable life for your pets. Experienced sales consultants
+                ready to answer all your questions. All pet products and food undergo mandatory and thorough testing.
+                We work only with official feed distributors, which is why we can guarantee quality.
+                A flexible system of discounts is provided for regular customers. Our salon employs professional
+                groomers who know how to approach your pets. Possessing knowledge of the anatomy and psychology of animals.
+                We work only with professional cosmetics. Dogs trained in our salon become
+                exhibition winners. The beauty and health of your pets fur is in our safe hands!',
             'image' => __DIR__ . '/img/08.jpeg',
             'employee' => [
                 0 => [
-                    'userName' => 'Азамат',
-                    'password' => 'азамат',
+                    'userName' => 'Azamat',
+                    'password' => 'azamat',
                     'phone' => '+998 (90) 222-12-12',
                     'price' => '140000',
                     'image' => __DIR__ . '/img/6.JPG',
                 ],
                 1 => [
-                    'userName' => 'Севинч',
-                    'password' => 'севинч',
+                    'userName' => 'Sevinch',
+                    'password' => 'sevinch',
                     'phone' => '+998 (90) 222-20-20',
                     'price' => '150000',
                     'image' => __DIR__ . '/img/7.JPG',
                 ],
                 2 => [
-                    'userName' => 'Диана',
-                    'password' => 'диана',
+                    'userName' => 'Diana',
+                    'password' => 'diana',
                     'phone' => '+998 (90) 222-30-30',
                     'price' => '160000',
                     'image' => __DIR__ . '/img/8.JPG',
